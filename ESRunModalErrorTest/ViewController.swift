8f9532f88cd6e2d537ab435af89fe860840d99bf
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-08 23:34:58 +0900

    Initial Commit

---
+++ ESRunModalErrorTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ESRunModalErrorTest
+//
+//  Created by Tomohiro Kumagai on H27/10/08.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
